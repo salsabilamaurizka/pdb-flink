351398205c82ebf96db46c3c217f9ec9426cec4f
salsabilamrzkaa <salsabila.maurizka@amartha.com>
2020-12-27 23:53:02 +0700

    modify settings

--- flink/settings.py
+++ flink/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'flink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
